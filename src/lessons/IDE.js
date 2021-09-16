32ff3664d4eb7a08d274c561916ff9a50e57323e
Jd109-del <61640807+Jd109-del@users.noreply.github.com>
2021-09-17 00:08:46 +0800

    modify2

--- src/lessons/IDE.js
+++ src/lessons/IDE.js
@@ -3,7 +3,7 @@ import TabsHtml from '../components/tabsHtml'
 
 
 const IDE = () => {
-
+    const [isEnabled, setIsEnabled] = useState(true);
  
     return (
         <>
@@ -12,18 +12,13 @@ const IDE = () => {
                     <div className=''>
                         <h1 className='headerUno'>IDE</h1>
                     </div>
-                <div toggle={toggle}>
-                    <div className={isEnabled ? 'hidden' : 'cursor-pointer '} onClick={toggle}>
+                <div>
+                    <div className={isEnabled ? 'cursor-pointer' : 'hidden'} >
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="blue">
                     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd" />
                     </svg>
                     </div>
-                    <div className={isEnabled ? 'hidden' : 'cursor-pointer '} onClick={toggle}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clipRule="evenodd" />
-                    </svg>
-                    </div>
-                <div className={isEnabled? 'block text-left' : 'hidden'}>
+                <div className={isEnabled ? 'block text-left' : 'hidden'}>
                 <TabsHtml />
                 </div>
                 </div>
