32e883e97f6f54ad23eed8f49b6fd89fad1bc8e6
Jd109-del <61640807+Jd109-del@users.noreply.github.com>
2021-09-24 13:14:28 +0800

    almost finish in nav and hero

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -7,6 +7,7 @@ import { HeroContainer,
     HeroContent
  } from './HeroElements'
  import Video from '../../videos/Abstract - 41940.mp4'
+ import { Link } from 'react-router-dom'
 
 const HeroSection = () => {
     return (
@@ -17,9 +18,12 @@ const HeroSection = () => {
             <HeroContent>
                 <HeroH1>HTML HUB</HeroH1>
                 <HeroBtnWrapper>
-                    <button to='/'>
-                        Aral na!
-                    </button>
+                <Link to='/Html' className='bg-blue-600 p-4 rounded-full text-white flex gap-2 transition duration-300 animate-bounce ease-in-out hover:bg-blue-300'>
+                        Aralin na!
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                        </svg>
+                    </Link>
                 </HeroBtnWrapper>
             </HeroContent>
         </HeroContainer>
