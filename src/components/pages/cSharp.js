f2450047e82d70b8ea4d58f2dbfd120d2de5eba8
Jd109-del <61640807+Jd109-del@users.noreply.github.com>
2021-10-13 14:33:06 +0800

    csharp page added but not accessible

---
+++ src/components/pages/cSharp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const cSharp = () => {
+    return (
+        <>
+           <div className='bg-white h-screen grid grid-cols-1'>
+                <div>
+                    <h1>
+                        C Sharp
+                    </h1>
+                </div>
+            </div> 
+        </>
+    )
+}
+
+export default cSharp
